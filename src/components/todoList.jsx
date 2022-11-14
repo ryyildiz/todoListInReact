bf15a80e4686b8d5eb2ea0d6448b0dd24216bedc
piaryildiz <Rumeysa.Yildiz@vodafone.com>
2022-11-14 09:28:53 +0300

    do todo list page

---
+++ src/components/todoList.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+function List() {
+    return (
+        <div className="todo">
+            <ul className="todo-list">
+
+            </ul>
+        </div>
+    )
+}
+
+export default List;
